5cb90565894c3863a47bfb0da1976c8d3463ef77
JSander72 <jamesearlsanders2@outlook.com>
2023-12-17 07:49:02 -0500

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const apiKey = "44a0e5165b265ba57bfd8e1d4165b730b";
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=birmingham";
+window.onload = function () {
+  const apiKey = "44a0e5165b265ba57bfd8e1d4165b730b";
+  const apiUrl = "http://api.openweathermap.org/data/2.5/weather?";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + '&appid=${apiKey}');
-  var data = await response.json();
+  const searchBox = document.querySelector('.search input');
+  const searchBtn = document.querySelector('.search button');
 
-  console.log(data);
-}
+  async function checkWeather(city) {
+    const response = await fetch(apiUrl + 'appid=' + apiKey + '&units=imperial' + '&q=' + city);
+    var data = await response.json();
 
-checkWeather();
+    console.log(data);
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' mi/hr';
+  }
+// not able to get search img to update city with api to refresh app - solve 
+  searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+  });
+};
