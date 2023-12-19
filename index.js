5d2e80a004bfc3075e74a471ec2da81607234b18
JSander72 <jamesearlsanders2@outlook.com>
2023-12-19 17:37:06 -0500

    second commit

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ window.onload = function () {
 
   const searchBox = document.querySelector('.search input');
   const searchBtn = document.querySelector('.search button');
+  const weatherIcon = document.querySelector('.weather-icon');
 
   async function checkWeather(city) {
-    const response = await fetch(apiUrl + 'APPID=' + apiKey + '&units=imperial&q=' + city);
-    var data = await response.json();
+    const response = await fetch(apiUrl + 'APPID=' + apiKey + '&units=imperial&q=' + city );
+    const data = await response.json();
 
     console.log(data);
 
@@ -15,6 +16,28 @@ window.onload = function () {
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + ' mi/hr';
+
+    if (data.weather[0].main=='Clouds'){
+      weatherIcon.src = 'images/clouds.png';
+    }
+    else if(data.weather[0].main=='Clear'){
+      weatherIcon.src = 'images/clear.png';
+    }
+    else if(data.weather[0].main=='Rain'){
+      weatherIcon.src = 'images/rain.png';
+    }
+    else if(data.weather[0].main=='Drizzle'){
+      weatherIcon.src = 'images/drizzle.png';
+    }
+    else if(data.weather[0].main=='Mist'){
+      weatherIcon.src = 'images/mist.png';
+    }
+    else if(data.weather[0].main=='Snow'){
+      weatherIcon.src = 'images/snow.png';
+    }
+
+
+
   }
 // not able to get search img to update city with api to refresh app - solved - button "type" added to html
   searchBtn.addEventListener('click', () => {
